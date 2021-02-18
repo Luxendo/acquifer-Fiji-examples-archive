1fbb2012f3fa82a981b31992ab42c0dbf81df68e
Laurent Thomas <laurent132.thomas@laposte.net>
2021-02-18 12:12:33 +0100

    add makeHyperstack examples

---
+++ examples/MakeHyperstack/makeHyperstack-im03.py
@@ -0,0 +1,43 @@
+"""
+From a directory, a well and subposition, this scripts list the image files and make a hyperstack for a selected well and subposition.
+the following variables should be updated to match your local image dataset:
+- inputDir
+- selectedWell and selecteSubpositions
+- listChannel, Zslice and Timepoint
+"""
+from acquifer.im03         import FileUtils, MetadataParser
+from acquifer.im03.plugins import Hyperstack_Maker_IM03
+
+inputDir = r"C:\Users\Laurent Thomas\Documents\Acquifer\DataSet\Fish\IM03_BISCHOFF_DORSAL_2ndGO_4x" # IM03
+
+# Applying a dimension filter
+selectedWell = ["A002"]
+selectedSubpositions = [1]
+listChannel   = None # if None or [], all available channels will be displayed, same for other dimensions
+listZslice    = None
+listTimepoint = None
+
+def printAligned(array):
+	"""
+	This function is designed to print each elements of an array on a new line.
+	""" 
+	for i in array:
+		print i
+
+listFiltered = FileUtils().getListImageFiles(inputDir, 
+                                             selectedWell, 
+                                             selectedSubpositions, 
+                                             listChannel, 
+                                             listZslice, 
+                                             listTimepoint)
+print "\nList files"
+printAligned(listFiltered)
+
+# Get metadatas
+listInfos = MetadataParser().getImagesInfos(listFiltered)
+print "\nImages metadata"
+printAligned(listInfos)
+
+# Make hyperstacks
+hyperstack = Hyperstack_Maker_IM03().makeHyperStack(listInfos)
+hyperstack.show()
---
+++ examples/MakeHyperstack/makeHyperstack-im04.py
@@ -0,0 +1,43 @@
+"""
+From a directory, a well and subposition, this scripts list the image files and make a hyperstack for a selected well and subposition.
+the following variables should be updated to match your local image dataset:
+- inputDir
+- selectedWell and selecteSubpositions
+- listChannel, Zslice and Timepoint
+"""
+from acquifer.im04         import FileUtils, MetadataParser
+from acquifer.im04.plugins import Hyperstack_Maker_IM04
+
+inputDir = r"C:\Users\Laurent Thomas\Documents\Acquifer\DataSet\Fish\Clicking_AppNote96" # IM04
+
+# Select one well and subposition
+selectedWell = ["A002"]
+selectedSubpositions = [1] # first subposition
+listChannel   = None # if None or [], all available channels will be displayed, same for other dimensions
+listZslice    = None
+listTimepoint = None
+
+def printAligned(array):
+	"""
+	This function is designed to print each elements of an array on a new line.
+	""" 
+	for i in array:
+		print i
+
+listFiltered = FileUtils().getListImageFiles(inputDir, 
+                                             selectedWell, 
+                                             selectedSubpositions, 
+                                             listChannel, 
+                                             listZslice, 
+                                             listTimepoint)
+print "\nList files"
+printAligned(listFiltered)
+
+# Get metadatas
+listInfos = MetadataParser().getImagesInfos(listFiltered)
+print "\nImages metadata"
+printAligned(listInfos)
+
+# Make hyperstacks
+hyperstack = Hyperstack_Maker_IM04().makeHyperStack(listInfos)
+hyperstack.show()
