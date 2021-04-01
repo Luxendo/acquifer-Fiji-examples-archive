b1b2dc9bc284b97d48124bba3c248ce140c18253
Laurent Thomas <laurent132.thomas@laposte.net>
2021-04-01 17:12:03 +0200

    Create groupImageFiles.py

---
+++ examples/jython/groupImageFiles.py
@@ -0,0 +1,26 @@
+"""
+From a list of images and associated metadatas, group images belonging to the same well positions
+The result is a list containing one sublists per well subpositions.
+A sublist contains the images belinging to a given well position
+"""
+#@ File (label="Dataset directory", style="directory") input_directory
+
+from acquifer.im04 import FileUtils, MetadataParser 
+from acquifer.im   import Metadatas
+
+def printSubLists(mainList): 
+	"""
+	This function takes a list of list and print each item of the sublist  on a new line. 
+	""" 
+	for subList in mainList: 
+		print "\n#Next subList" 
+		 
+		for item in subList: 
+			print item 
+
+utils = FileUtils()
+input_directory = input_directory.getPath()
+listMetadatas = utils.getListMetadatas(input_directory) # Get Metadatas 
+groupedImages = Metadatas.groupByWellPositions(listMetadatas) 
+
+printSubLists(groupedImages)
