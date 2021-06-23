a041bf11f1a7d202e3b6eda5208b96b43f78d828
Laurent Thomas <laurent132.thomas@laposte.net>
2021-06-23 16:51:02 +0200

    Update testDataset.py

--- examples/jython/testDataset.py
+++ examples/jython/testDataset.py
@@ -1,27 +1,47 @@
-from acquifer.core.im04 import ImagePlane, Dataset
+#@ File (label="Select an IM directory", style="directory") directory
+from acquifer.core import Dataset
+from acquifer.core.im04 import ImagePlane, DatasetBuilder
+from acquifer.core.im import ImagePlane as Sorting # Make an enum out of those
+
 from acquifer.ij import Utils 
 from ij import IJ
 
-# Unfiltered dataset, get all images
-dataset = Dataset(r"C:\Users\Laurent Thomas\Documents\Acquifer\DataSet\Hanh\Lateral\subSet4X")
-print dataset
+# Unfiltered and unsorted dataset, get all images
+dataset1 = DatasetBuilder(directory).build()
 
-Utils.printArray(dataset.getListImagePlanes())
+#dataset = Dataset(builder)
+print "Dataset unfiltered", dataset1
 
-# Filter the List<Metdata> 
-filterWells        = ["B001","B002"]
-#filterWells        = [] # empty list as filter should return all wells
+
+
+## WITH DIMENSIONS FILTER
+#filterWells        = ["A001","A002"]
+filterWells       = [] # empty list as filter should return all wells
 filterSubPositions = []
-filterChannels     = [8]
-filterZslices      = []
-filterTimepoints   = []
+filterChannels     = []
+filterZ            = []
+filterTimepoints   = [1,2]
 
-dataset2 = Dataset(r"C:\Users\Laurent Thomas\Documents\Acquifer\DataSet\Hanh\Lateral\subSet4X", filterWells,
-																								filterSubPositions,
-																								filterChannels,
-																								filterZslices,
-																								filterTimepoints)
+# Define custom sorting
+#sortOrder = None
+#sortOrder = Sorting.SORT_WELL
+#sortOrder = Sorting.SORT_SUBPOSITION
+#sortOrder = Sorting.SORT_CHANNEL
+#sortOrder = Sorting.SORT_Z
+#sortOrder = Sorting.SORT_TIMEPOINT
+sortOrder = Sorting.SORT_DEFAULT
 
-IJ.log("\nWith filters")
-print dataset2
+# Unfiltered and unsorted dataset, get all images
+# Use backslashes \ to spread the command on multiple lines
+dataset2 = DatasetBuilder(directory).includeWellIds(filterWells) \
+									.includeSubpositions(filterSubPositions) \
+								    .includeChannels(filterChannels) \
+								    .includeZ(filterZ) \
+								    .includeTimepoints(filterTimepoints) \
+								    .sortBy(sortOrder) \
+								    .build()
+
+#dataset = Dataset(builder)
+print "Dataset filtered", dataset2
+IJ.log("\nlist of images")
 Utils.printArray(dataset2.getListImagePlanes())
